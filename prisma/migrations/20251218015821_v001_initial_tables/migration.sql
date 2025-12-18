e9cc1d1be3520517edd05ca40c8dad1d76e7b95b
Baggio25 <rodrigo.baggio.si@gmail.com>
2025-12-17 22:59:53 -0300

    chore: fix setup prisma and create initial tables

--- prisma/migrations/20251218015821_v001_initial_tables/migration.sql
+++ prisma/migrations/20251218015821_v001_initial_tables/migration.sql
@@ -34,7 +34,7 @@ CREATE TABLE "SaleProduct" (
 );
 
 -- AddForeignKey
-ALTER TABLE "SaleProduct" ADD CONSTRAINT "SaleProduct_saleId_fkey" FOREIGN KEY ("saleId") REFERENCES "Sale"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "SaleProduct" ADD CONSTRAINT "SaleProduct_saleId_fkey" FOREIGN KEY ("saleId") REFERENCES "Sale"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "SaleProduct" ADD CONSTRAINT "SaleProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "SaleProduct" ADD CONSTRAINT "SaleProduct_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
