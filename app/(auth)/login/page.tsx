b7e60c59b3ef0a13b8530cede2a71d1cb472bbe7
Baggio25 <rodrigo.baggio.si@gmail.com>
2026-05-03 18:29:57 -0300

    feat: add initial pages

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+const LoginPage = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="border-[0.5px] border-gray-500 p-4 w-96 rounded-md">
+        <h1 className="text-green-500 text-2xl font-black">STOCKLY</h1>
+        <h2 className="text-gray-500 font-semibold">Acesso ao sistema</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
