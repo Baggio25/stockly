63f0c1268c7764f4ec2c2f5ca6a1c3a5ce97cb50
Baggio25 <rodrigo.baggio.si@gmail.com>
2026-05-03 18:29:36 -0300

    chore: add initial tailwind and prettier setup

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { Inter } from "next/font/google";
+import Sidebar from "./_components/sidebar";
+
+const inter = Inter({ subsets: ["latin"], display: "auto" });
+
+export const metadata: Metadata = {
+  title: "Stockly",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-Br">
+      <body className={`${inter.className} antialiased`}>
+        <div className="flex h-full gap-8">
+          <Sidebar />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
